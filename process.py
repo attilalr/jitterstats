94bcec2385b7c2450e3d04017476afddf5b97666
attilalr <attila.leaes@gmail.com>
2016-11-19 14:55:44 -0200

    Sampling normal distribution for comparison now is optional and must be processed separately. All the tests are done with the normal distribution too.

--- process.py
+++ process.py
@@ -1,12 +1,9 @@
 import numpy as np
 import random
 import sys
-import matplotlib.pyplot as p
 from multiprocessing import Pool
 
 calculate_gaussian=0
-
-print sys.argv
 
 if (len(sys.argv)!=2 and len(sys.argv)!=3):
   print """
@@ -63,37 +60,30 @@ am_work=20000000
 
 def simulate((calc_gauss,N,nsamples,datahist,nbins,size_bin,mean,std)):
   teste=list()
-  teste_gauss=list()
   sum_tmax=0
   idleness=0
   comp_time=0
   
   for i in range(nsamples):
-    sample_list=np.random.choice(nbins,size=N,p=np.array(datahist)*size_bin)
+    if calc_gauss==0: # use profile distribution
+      sample_list=np.random.choice(nbins,size=N,p=np.array(datahist)*size_bin)
+    if calc_gauss==1: # use normal distribution with mean and std based on profile
+      sample_list=std*np.random.randn(N)+mean
+
     tmax=sample_list.max()
+ 
     sum_tmax=sum_tmax+tmax # sum tmax, effective computing time
     idleness=idleness+(N*tmax-sample_list.sum())/(N*tmax)
     comp_time=comp_time+sample_list.sum()/(N*tmax)
     
     teste.append(tmax)
-    if calc_gauss==1:
-      teste_gauss.append((std*np.random.randn(N)+mean).max())
     
-
-  sum_tmax=sum_tmax    
   mean_idleness=idleness/nsamples
   mean_eff_comp=comp_time/nsamples
   
   teste=np.array(teste)
-  if calc_gauss==1:
-    teste_gauss=np.array(teste_gauss)
 
-  if calc_gauss==1:
-    return [N,teste.mean(),teste.std(),teste_gauss.mean(),teste_gauss.std(),sum_tmax,mean_idleness,mean_eff_comp]
-
-  if calc_gauss==0:
-    return [N,teste.mean(),teste.std(),0,0,sum_tmax,mean_idleness,mean_eff_comp]
-
+  return [N,teste.mean(),teste.std(),sum_tmax,mean_idleness,mean_eff_comp]
 
 #creating the entry parameters
 #N_list=[x for x in range(10,100,10)]+[x for x in range(100,1000,100)]+[x for x in range(1000,10000,1000)]
@@ -105,20 +95,12 @@ for n in N_list:
 pool=Pool(processes=len(N_list))
 results=pool.map(simulate,parameters)
 
-#print mean, std
-#for i in results:
-#  print i
-#  datahist_tmp,bins_tmp = np.histogram(teste,bins=100,density=True)
-#  color=random.choice(['r','g','b','y'])
-#  p.plot(bins_tmp[:-1,],datahist_tmp,color+'o--')
-#p.show()
-
-t0=results[0][5]
+t0=results[0][3]
 n0=results[0][0]
 
 print "# "+str(mean)+" "+str(std)
-print "# N | slowdown | sigma | slowdon_gaussian | sigma-slowdown_gaussian | t_eff_finish | ideal_t_eff_finish "
+print "# N | slowdown | sigma | t_eff_finish | ideal_t_eff_finish | mean idleness | mean effective computing"
 for res in results:
-  print res[0], ((res[1]-mean)/mean)*100, ((res[1]+res[2]-mean)/mean)*100-((res[1]-mean)/mean)*100, ((res[3]-mean)/mean)*100, ((res[3]+res[4]-mean)/mean)*100-((res[3]-mean)/mean)*100,res[5], (t0*n0)/res[0], res[6], res[7]
+  print res[0], ((res[1]-mean)/mean)*100, ((res[1]+res[2]-mean)/mean)*100-((res[1]-mean)/mean)*100,res[3]/t0, (t0*n0)/(res[0]*t0), res[4], res[5]
 
 file.close()
